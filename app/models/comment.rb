5349796111de8221d326c4f6f1d3ddc811c24723
royc <royc10000@gmail.com>
2017-12-15 19:16:02 +0800

    Add: added comment.

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  belongs_to :post
+  validates_presence_of :post_id
+  validates_presence_of :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215105504) do
+ActiveRecord::Schema.define(version: 20171215111129) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
