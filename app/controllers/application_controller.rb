ce675bb58c06f2f04850f623a2502a22266cc493
royc <royc10000@gmail.com>
2017-12-17 02:25:25 +0800

    Fix: fixed get user email by id.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def get_email(author_id)
-    @current_user ||= Author.find_by_id!(author_id)
+    @current_user = Author.find_by_id!(author_id)
   end
   helper_method :get_email
 end
