5f1614f4fb19f6e8ac19d191464d2f6ecd454597
royc <royc10000@gmail.com>
2017-12-17 03:19:22 +0800

    Update: set only login for Facebook.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
     @current_user = Author.find_by_id!(author_id)
   end
   helper_method :get_email
+
+  def authenticate_author!
+    if author_signed_in?
+      super
+    else
+      redirect_to author_facebook_omniauth_authorize_url ## , :notice => 'if you want to add a notice'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
